417f9baf6a318bc909875ef9635661f4ff0b1503
Steven Almeida <pyro1357@outlook.com>
2023-12-12 00:10:56 -0500

    Added the canvas and the ability to draw on it

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const GRID_SIZE = 16;
-
+const COLOR_LIST = ["black", "red", "green", "blue", "eraser"];
+const canvas = document.querySelector("#canvas");
 const colorButtons = document.querySelectorAll(".color-option");
-const eraserButton = document.querySelector("#eraser");
 let selectedColor;
+let mouseDown = false;
+let grid = [];
+
+/* TODO: Make selectedColor be a string with the color value
+         Load color selector with colors from the color list
+         Add a clear canvas button
+*/
+
+document.body.onmousedown = function(e)
+{
+    mouseDown = true;
+    e.preventDefault();
+}
+document.body.onmouseup = function() { mouseDown = false; }
 
 function changeColor(newColor)
 {
@@ -23,6 +37,7 @@ function changeColor(newColor)
     selectedColor = newColor;
 }
 
+// SETTING UP THE COLOR BUTTONS
 for (let i = 0; i < colorButtons.length; i++)
 {
     const button = colorButtons[i];
@@ -30,4 +45,33 @@ for (let i = 0; i < colorButtons.length; i++)
         changeColor(button);
     });
 }
+// SET BLACK AS THE STARTING COLOR
 changeColor(colorButtons[0]);
+
+// SETTING UP THE GRID
+for (let x = 0; x < GRID_SIZE; x++)
+{
+    grid[x] = [];
+    for (let y = 0; y < GRID_SIZE; y++)
+    {
+        const newNode = document.createElement("div");
+        newNode.style.height = (1/GRID_SIZE) * 100 + "%";
+        newNode.style.width = (1/GRID_SIZE) * 100 + "%";
+        canvas.appendChild(newNode);
+        
+        newNode.addEventListener("mouseover", () => {
+            if (!mouseDown) return;
+            const color = selectedColor.getAttribute("id")
+            if (newNode.classList.contains(color)) return;
+            newNode.classList.remove(...COLOR_LIST);
+            newNode.classList.add(color);
+        });
+
+        newNode.addEventListener("mousedown", () => {
+            const color = selectedColor.getAttribute("id")
+            if (newNode.classList.contains(color)) return;
+            newNode.classList.remove(...COLOR_LIST);
+            newNode.classList.add(color);
+        });
+    }
+}
