af1a750498ce10d0c052dfdc997cf039f7ffaa51
Steven Almeida <pyro1357@outlook.com>
2023-12-13 00:39:54 -0500

    Made color pallete generate from code instead of already being in HTML

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const GRID_SIZE = 16;
-const COLOR_LIST = ["black", "red", "green", "blue", "eraser"];
 const canvas = document.querySelector("#canvas");
+const pallete = document.querySelector("#pallete");
 const colorButtons = document.querySelectorAll(".color-option");
 const clearButton = document.querySelector("#clear-button");
 let selectedColor;
 let mouseDown = false;
 let grid = [];
+let colorList = new Map();
+colorList.set("black", "#000000");
+colorList.set("red", "#f51414");
+colorList.set("green", "#19de12");
+colorList.set("blue", "#0e15e6");
+colorList.set("white", "#e6f0f5");
 
-/* TODO: Make selectedColor be a string with the color value
-         Load color selector with colors from the color list
-         Add a clear canvas button
-*/
+// TODO: Make an Add Color button that allows for custom colors in the pallete
 
 document.body.onmousedown = function(e)
 {
@@ -24,10 +27,19 @@ function clearCanvas()
     const canvasNodes = canvas.children;
     for (node of canvasNodes)
     {
-        node.classList.remove(...COLOR_LIST);
-        node.classList.add("eraser");
-
+        node.style.backgroundColor = colorList.get("white");
     }
+}
+
+function getColorSelectionClass(bgColor)
+{
+    var color = (bgColor.charAt(0) === '#') ? 
+        bgColor.substring(1, 7) : bgColor;
+    var r = parseInt(color.substring(0, 2), 16); // hexToR
+    var g = parseInt(color.substring(2, 4), 16); // hexToG
+    var b = parseInt(color.substring(4, 6), 16); // hexToB
+    return (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 186) ?
+        "selected-black" : "selected";
 }
 
 function changeColor(newColor)
@@ -35,30 +47,27 @@ function changeColor(newColor)
     if (selectedColor === newColor) return;
     if (selectedColor !== undefined)
     {
-        selectedColor.classList.remove("selected", "selected-black");
+        const colorButton = document.querySelector("#"+selectedColor);
+        colorButton.classList.remove("selected", "selected-black");
     }
-
-    if (newColor.getAttribute('id') === "eraser")
-    {
-        newColor.classList.add("selected-black");
-    }
-    else
-    {
-        newColor.classList.add("selected");
-    }
+    const colorButton = document.querySelector("#"+newColor);
+    colorButton.classList.add(getColorSelectionClass(colorList.get(newColor)));
     selectedColor = newColor;
 }
 
-// SETTING UP THE COLOR BUTTONS
-for (let i = 0; i < colorButtons.length; i++)
+// GENERATE COLOR PALLETE
+for (let [color, hexCode] of colorList)
 {
-    const button = colorButtons[i];
-    button.addEventListener("click", () => {
-        changeColor(button);
+    const palleteButton = document.createElement("div");
+    palleteButton.classList.add("color-option");
+    palleteButton.setAttribute("id", color);
+    palleteButton.style.backgroundColor = hexCode;
+    palleteButton.addEventListener("click", () => {
+        changeColor(color);
     });
+    pallete.appendChild(palleteButton);
 }
-// SET BLACK AS THE STARTING COLOR
-changeColor(colorButtons[0]);
+changeColor("black");
 
 // SETTING UP THE GRID
 for (let x = 0; x < GRID_SIZE; x++)
@@ -69,23 +78,17 @@ for (let x = 0; x < GRID_SIZE; x++)
         const newNode = document.createElement("div");
         newNode.style.height = (1/GRID_SIZE) * 100 + "%";
         newNode.style.width = (1/GRID_SIZE) * 100 + "%";
-        newNode.classList.add("eraser");
+        newNode.style.backgroundColor = colorList.get("white");
         canvas.appendChild(newNode);
         grid[x][y] = newNode;
         
         newNode.addEventListener("mouseover", () => {
             if (!mouseDown) return;
-            const color = selectedColor.getAttribute("id")
-            if (newNode.classList.contains(color)) return;
-            newNode.classList.remove(...COLOR_LIST);
-            newNode.classList.add(color);
+            newNode.style.backgroundColor = colorList.get(selectedColor);
         });
 
         newNode.addEventListener("mousedown", () => {
-            const color = selectedColor.getAttribute("id")
-            if (newNode.classList.contains(color)) return;
-            newNode.classList.remove(...COLOR_LIST);
-            newNode.classList.add(color);
+            newNode.style.backgroundColor = colorList.get(selectedColor);
         });
     }
 }
